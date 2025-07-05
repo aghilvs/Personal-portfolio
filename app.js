3681ead0a51b33bb829a1f08ab1703e4ee8bec29
aghilvs <aghilvs@gmail.com>
2025-07-05 06:41:00 +0000

    Disabled some of the functionalities in the javascript, Added some comments in the html file, Made some minor changes to the anmiation in 'active' class

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ function pageTransitions() {
         const id = e.target.dataset.id;
         if(id) {
             //Remove Selected from the other buttons
-            sectBtns.forEach((btn) => {
-                btn.classList.remove('active')
-            })
-            e.target.classList.add('active')
+            // sectBtns.forEach((btn) => {
+            //     btn.classList.remove('active') // Check whether it is necessary or not
+            // })
+            // e.target.classList.add('active') // Better not to give 'active' to the controll icons
             //hide other sections
             sections.forEach((section)=> {
                 section.classList.remove('active')
